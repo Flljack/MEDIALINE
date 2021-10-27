64c9c79357d9ea25a7e640cef38df9554eb367dc
Flljack <ryazanovpr@gmail.com>
2021-10-27 22:16:17 +0300

    add route and controller newsList

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\News;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        return view('newsFeed', ['newsList' => News::orderBy('created_at')->simplePaginate(5)]);
+    }
+}
---
+++ resources/views/newsFeed.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('title', 'Новости')
+
+@section('content')
+    @foreach ($newsList as $news)
+        <div class="card">
+            <div class="card-info">
+                <div class="card-date">
+                <span>
+					{{\Carbon\Carbon::parse($news->created_at)->format('d/m/Y H:i')}}
+				</span>
+                </div>
+                <h6>{{$news->author}}</h6>
+                <h2>{{$news->title}}</h2>
+                <p>{{ Str::limit($news->description, 200) }}</p>
+                <a class="card__show-more" href="#">подробнее</a>
+
+            </div>
+        </div>
+    @endforeach
+    {{ $newsList->links() }}
+@endsection
