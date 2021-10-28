2a7f3096eb615974e27ee202167cfaa467cd29b7
Flljack <ryazanovpr@gmail.com>
2021-10-28 13:53:28 +0300

    add responsive

--- resources/views/newsFeed.blade.php
+++ resources/views/newsFeed.blade.php
@@ -3,21 +3,23 @@
 @section('title', 'Новости')
 
 @section('content')
-    @foreach ($newsList as $news)
-        <div class="card">
-            <div class="card-info">
-                <div class="card-date">
-                <span>
-					{{\Carbon\Carbon::parse($news->created_at)->format('d/m/Y H:i')}}
-				</span>
-                </div>
-                <h6>{{$news->author}}</h6>
-                <h2>{{$news->title}}</h2>
-                <p>{{ Str::limit($news->description_preview, 200) }}</p>
-                <a class="card__show-more" href="{{route('news', $news->id)}}">подробнее</a>
+    <div class="news-feed">
+        @foreach ($newsList as $news)
+            <div class="card">
+                <div class="card-info">
+                    <div class="card-date">
+                    <span>
+                        {{\Carbon\Carbon::parse($news->created_at)->format('d/m/Y H:i')}}
+                    </span>
+                    </div>
+                    <h6>{{$news->author}}</h6>
+                    <h2>{{$news->title}}</h2>
+                    <p>{{ Str::limit($news->description_preview, 200) }}</p>
+                    <a class="card__show-more" href="{{route('news', $news->id)}}">подробнее</a>
 
+                </div>
             </div>
-        </div>
-    @endforeach
-    {{ $newsList->links() }}
+        @endforeach
+        {{ $newsList->links() }}
+    </div>
 @endsection
